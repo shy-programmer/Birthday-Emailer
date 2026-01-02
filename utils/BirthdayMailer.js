fe72d7b770cb518771af64967ffef66f0aca100f
Arowolo Abdulazeez <abdulazeezarowolo@gmail.com>
2026-01-02 18:38:37 +0100

    test/ testing nodemailer with render

--- utils/BirthdayMailer.js
+++ utils/BirthdayMailer.js
@@ -1,4 +1,5 @@
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
+const sgMail = require('@sendgrid/mail');
 const schedule = require('node-schedule');
 // const { google } = require("googleapis");
 const celebrantModel = require('../birthdayApi/birthday.model')
@@ -6,6 +7,8 @@ const dotenv = require('dotenv')
 dotenv.config();
 const fs = require('fs');
 const path = require("path");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const SENDER = process.env.MY_MAIL
 // const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
@@ -20,34 +23,34 @@ const SENDER = process.env.MY_MAIL
 // );
 // oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-async function createTransporter() {
-    try {
+// async function createTransporter() {
+//     try {
 
         // const accessToken = await oAuth2Client.getAccessToken(); // Automatically refresh token
         // if (!accessToken.token) {
         //     throw new Error("Failed to retrieve access token");
         // }
-        return nodemailer.createTransport({
-            service: "gmail",
+        // return nodemailer.createTransport({
+        //     service: "gmail",
             // host: "smtp.gmail.com",
             // scope: "https://mail.google.com/",
             // port: 587,
             // secure: true,
-            auth: {
+            // auth: {
                 // type: "OAuth2",
-                user: SENDER,
-                pass: process.env.GOOGLE_APP_PASSWORD,
+                // user: SENDER,
+                // pass: process.env.GOOGLE_APP_PASSWORD,
                 // clientId: process.env.GOOGLE_CLIENT_ID,
                 // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
                 // refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
                 // accessToken: accessToken.token,
-            },
-        });
-    } catch (error) {
-        console.error("Failed to refresh access token:", error);
-        return null;
-    }
-}
+//             },
+//         });
+//     } catch (error) {
+//         console.error("Failed to refresh access token:", error);
+//         return null;
+//     }
+// }
 
 // const transporter = nodemailer.createTransport({
 //     service: "gmail",
@@ -101,21 +104,42 @@ const birthdayChecker = async () => {
     }
 }
 
+// const sendBirthdayEmail = async (email, username) => {
+//     try {
+//         const htmlContent = getBirthdayTemplate(username);
+//         const transporter = await createTransporter()
+
+//         transporter.sendMail({
+//             "from": `"Birthday-Emailer" ${SENDER}`,
+//             "to": `${email}`,
+//             "subject": `Hapy Birthday! ${username}`,
+//             "html": htmlContent
+//         })
+//     } catch (err) {
+//         console.error(`Error sending email to ${email}:`, err);
+//     }
+// }
+
 const sendBirthdayEmail = async (email, username) => {
     try {
         const htmlContent = getBirthdayTemplate(username);
-        const transporter = await createTransporter()
 
-        transporter.sendMail({
-            "from": `"Birthday-Emailer" ${SENDER}`,
-            "to": `${email}`,
-            "subject": `Hapy Birthday! ${username}`,
-            "html": htmlContent
-        })
+        const msg = {
+            to: email,
+            from: {
+                email: SENDER,
+                name: 'Birthday Emailer'
+            },
+            subject: `Happy Birthday 🎉 ${username}`,
+            html: htmlContent
+        };
+
+        await sgMail.send(msg);
+        console.log(`Email sent to ${email}`);
     } catch (err) {
-        console.error(`Error sending email to ${email}:`, err);
+        console.error(`Failed to send to ${email}:`, err.response?.body || err.message);
     }
-}
+};
 
 const getBirthdayTemplate = (username) => {
     const filePath = path.join(__dirname, '../views/birthdayMail.html');
